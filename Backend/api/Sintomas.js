1c3d06b4329ee301212db6b67391023e4d4a2433
David Sanchez <davidademirsanchezcotrado@gmail.com>
2022-07-19 02:24:29 -0500

    Hambre y sueño es lo que tú tienes

--- Backend/api/Sintomas.js
+++ Backend/api/Sintomas.js
@@ -4,19 +4,16 @@ const router = express.Router();
 const Enfermedad = require("../models/Enfermedad");
 
 router.get("/getEnfermedadRel", (req, res) => {
-    // Se tiene 4 síntomas ingreesados y se compara con los síntomas de todas las enfermedades y te regresa la probabilidad
-    // probabilidad = (s1*0.15 + s2*0.15 + s3*0.25 + s4*0.15 + s5*0.13 + s6*0.1 + s7*0.05)
-    ssl = req.body;
-    // var sl = ["Estatura baja"];
-    var results = [];
+    lista_sintomas = req.body;
+    let results = [];
     async function obtProb() {
         for await (const doc of Enfermedad.find()) {
-            s = doc.sintomas;
-            var prob_sum = 0;
-            ssl.forEach((e) => {
-                s.forEach((se) => {
-                    if (se[0] == e) {
-                        prob_sum = prob_sum + se[1];
+            sintomas_db = doc.sintomas;
+            let prob_sum = 0;
+            lista_sintomas.forEach((sintomas_paciente) => {
+                sintomas_db.forEach((sintomas) => {
+                    if (sintomas[0] == sintomas_paciente) {
+                        prob_sum = prob_sum + sintomas[1];
                     }
                 });
             });
