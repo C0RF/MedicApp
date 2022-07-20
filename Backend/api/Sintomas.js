7b2f0047f7da2ae108f4fc6f31932cbe7244bc1e
ajulissa <ajulissa@gmail.com>
2022-07-20 00:13:48 -0500

    sintomas

--- Backend/api/Sintomas.js
+++ Backend/api/Sintomas.js
@@ -4,8 +4,11 @@ const router = express.Router();
 const Enfermedad = require("../models/Enfermedad");
 
 router.post("/enfermedadesRelacionadas", (req, res) => {
-
+    // Se tiene 4 síntomas ingreesados y se compara con los síntomas de todas las enfermedades y te regresa la probabilidad
+    // probabilidad = (s1*0.15 + s2*0.15 + s3*0.25 + s4*0.15 + s5*0.13 + s6*0.1 + s7*0.05)
     lista_sintomas = req.body.sintomas;
+    //console.log("ssl", lista_sintomas)
+    // var sl = ["Estatura baja"];
     var results = [];
     const removeAccents = (str) => {
         return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
